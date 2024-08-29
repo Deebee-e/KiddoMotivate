34cc80faed16aa77e6cd53832aef0d281930dad3
sir-george2500 <gsmulbah2500@gmail.com>
2024-08-29 17:53:39 +0000

    added style for the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
-
+"use client"
+import React from 'react';
+import Link from 'next/link';
 export default function NavBar() {
-  return (
-    <div>NavBar</div>
-  )
-}
 
+
+
+  return (
+    <div className="navbar fixed top-0 left-0 w-full bg-white/40 backdrop-blur-lg border-b border-gray-300/30 z-50">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">
+          <h4>Kiddo Motivate</h4>
+        </a>
+      </div>
+      <div className="flex-none">
+        <div className="menu menu-horizontal px-1">
+          <Link href="/"><button className="btn btn-active btn-ghost">
+            Logout
+          </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
