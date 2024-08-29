ffdb54362b5f3c677865fab902b56137da504efd
sir-george2500 <gsmulbah2500@gmail.com>
2024-08-29 18:29:36 +0000

    added style for the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,17 +6,20 @@ export default function NavBar() {
 
 
   return (
-    <div className="navbar fixed top-0 left-0 w-full bg-white/40 backdrop-blur-lg border-b border-gray-300/30 z-50">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">
-          <h4>Kiddo Motivate</h4>
-        </a>
-      </div>
-      <div className="flex-none">
-        <div className="menu menu-horizontal px-1">
-          <Link href="/"><button className="btn btn-active btn-ghost">
-            Logout
-          </button>
+    <div className="fixed top-0 left-0 w-full bg-white/40 backdrop-blur-lg border-b border-gray-300/30 z-50">
+      <div className="flex justify-between items-center p-4">
+        <div className="flex items-center space-x-4">
+          <h4 className="text-xl font-semibold">Kiddo Motivate</h4>
+
+        </div>
+        <div>
+          <Link href="/">
+            <button className="px-4 py-2  text-white rounded-md hover:bg-zinc-80 hover:text-black">
+              Sign Up
+            </button>
+            <button className="px-4 py-2  text-white rounded-md hover:bg-zinc-80 hover:text-black">
+              Login
+            </button>
           </Link>
         </div>
       </div>
