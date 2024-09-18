d0219e930f04a3e11bada18cb4c717a3f631f8db
sir-george2500 <gsmulbah2500@gmail.com>
2024-09-18 02:19:32 +0000

    added the sider code

--- src/app/affirrmation/page.tsx
+++ src/app/affirrmation/page.tsx
@@ -1,36 +1,63 @@
-"use client"
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+"use client";
+import React, { useState } from "react";
+import Icon from '@mdi/react';
+import { mdiChevronRight, mdiChevronLeft } from '@mdi/js';
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+export default function Affirmation() {
+  // Array of custom affirmations
+  const affirmations = [
+    "I am capable of achieving my goals.",
+    "Every day is a new opportunity to grow.",
+    "I am confident in my abilities.",
+    "I am worthy of happiness and success.",
+    "Challenges help me grow stronger.",
+    "I embrace change and welcome new beginnings."
+  ];
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+  const [currentSlide, setCurrentSlide] = useState(0);
 
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) =>
+      prevSlide === affirmations.length - 1 ? 0 : prevSlide + 1
+    );
+  };
 
-export default function Affirrmation() {
+  const prevSlide = () => {
+    setCurrentSlide((prevSlide) =>
+      prevSlide === 0 ? affirmations.length - 1 : prevSlide - 1
+    );
+  };
+
   return (
-    <main className="min-h-screen  items-center justify-between p-24">
-
-      <Swiper
-        // install Swiper modules
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={50}
-        slidesPerView={3}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-      </Swiper>
+    <main className="flex min-h-screen flex-col items-center justify-center   p-12 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <div className="relative w-full max-w-xl text-center">
+        <div className="flex items-center justify-center">
+          {affirmations.map((affirmation, index) => (
+            <div
+              key={index}
+              className={`${index === currentSlide ? "flex" : "hidden"
+                } flex justify-center items-center h-40 text-lg font-semibold text-black`}
+            >
+              <h3>{affirmation}</h3>
+            </div>
+          ))}
+        </div>
+        {/* Previous Button */}
+        <button
+          className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-lg hover:bg-opacity-70"
+          onClick={prevSlide}
+        >
+          <Icon path={mdiChevronLeft} size={1.5} />
+        </button>
+        {/* Next Button */}
+        <button
+          className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-lg hover:bg-opacity-70"
+          onClick={nextSlide}
+        >
+          <Icon path={mdiChevronRight} size={1.5} />
+        </button>
+      </div>
     </main>
-  )
+  );
 }
 
